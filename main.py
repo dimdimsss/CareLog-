2d22b6e55e7de32ac276b90b22bbda56757947e0
Dmitry <ddmi0001@student.monash.edu>
2025-09-24 10:35:38 +1000

    first step

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-import jason
-print("hello world")
+class CareLog:
+    def __init__(self, patients_name, log, date_and_time):
+        self.patients_name = patients_name
+        self.log = log
+        self.date_and_time = date_and_time
