dca227af33742ccec9e0fd40d92272ad55cb6068
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-08 17:03:02 +1100

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,8 @@ class CareStaff:
 
 st.header("test header") # im testing to see if streamlit is imported correctly can you guys check to see if this is showing on your end. works for me but i have path issues so it might be different - Aidan
 
+#choosing a user type and then showing a different dashboard for each type (get from other folders/modules). what do you guys think about this approach?
+option = st.selectbox(
+    'Choose a color:',
+    ['CareStaff', 'Patient', 'Clerk', 'Admin']
+)
