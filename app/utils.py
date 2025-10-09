c4346e7ee4e77f7eea06eae81bc9871f5452b960
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-09 16:38:35 +1100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -5,15 +5,18 @@ def testfunction():
     print("function is working!")
 
 class User:
+    """placeholder docstring"""
     def __init__(self, user_id, name, role):
         self.user_id = user_id
         self.name = name
         self.role = role
 
 class Patient:
+    """placeholder docstring"""
     def __init__(self, mrn): # what does mrn mean?(UML diagram)
         self.mrn = mrn
 
 class CareStaff:
+    """placeholder docstring"""
     def __init__(self, speciality):
         self.speciality = speciality
