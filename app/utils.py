25d762226cc8c603bde9f88c50cd6beceb957cd3
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-10 12:42:29 +1100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,16 @@
 #"utility" file for functions. we can organise classes and other function better in other files later on
 import json
 
+
 def testfunction():
     print("function is working!")
+
+
+class TestClass:
+    """If you are seeing this message in streamlit, it means class imports are working"""
+    def __init__(self, field):
+        self.field = field
+
 
 class User:
     """placeholder docstring"""
