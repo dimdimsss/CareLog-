2fad21d7395174e6bc6cde278188e35a1866ba73
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-13 17:21:41 +1100

    Create patient.py

---
+++ app/patient.py
@@ -0,0 +1,7 @@
+class patient(User):
+    def __init__(self, user_id, password, name, role, symptoms="None", preferences="None", logs=None):
+        super().__init__(user_id, password, name, role)
+
+        self.symptoms = symptoms
+        self.preferences = preferences
+        self.logs = logs if logs is not None else []
