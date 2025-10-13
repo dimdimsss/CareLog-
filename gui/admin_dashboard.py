bd421ea103e087478146b2cff4828aac0f0ae2aa
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-14 09:15:16 +1100

    Update admin_dashboard.py

--- gui/admin_dashboard.py
+++ gui/admin_dashboard.py
@@ -11,6 +11,13 @@ def launch_admin_dashboard():
 
     # Fill this in with dashboard features and functions
 
+    st.subheader("View Patient Logs")
+    patient_for_view = st.text_input("Enter patient user ID")
+
+    loaded_log = app.utils.get_patient_logs(patient_for_view, "data/patient_data.json")
+
+    st.selectbox["Select a log", loaded_log]
+
 
 
     # Quit function to return to login page. Leave this at the bottom
