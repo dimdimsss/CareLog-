bf04ac2b04b3e4526e64cb62a3942a68c252f2ef
A1D4NA1D4N <150621297+A1D4NA1D4N@users.noreply.github.com>
2025-10-21 21:11:04 +1100

    Update admin_dashboard.py

--- gui/admin_dashboard.py
+++ gui/admin_dashboard.py
@@ -23,7 +23,12 @@ def launch_admin_dashboard():
             st.write(f"Viewing data for patient: {current_patient.name}")
             st.write(f"Symptoms: {current_patient.symptoms}")
             st.write(f"Preferences: {current_patient.preferences}")
-            st.write(f"Staff Notes: {current_patient.personal_notes}")
+            
+            with st.expander(f"Staff notes for {current_patient.name}", expanded=False):
+                for note in current_patient.personal_notes:
+                    for note_title, note_content in note.items():
+                        with st.expander(note_title, expanded=False):
+                            st.write(note_content)
             
             with st.expander(f"Logs for {current_patient.name}", expanded=False):
                 for log in current_patient.logs:
